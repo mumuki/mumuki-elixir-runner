89c53effd9653d4cac9d41b4984b60fcaf096c5b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-09-27 19:22:24 -0300

    Require's clenaup

--- lib/elixir_runner.rb
+++ lib/elixir_runner.rb
@@ -3,10 +3,10 @@ require 'mumukit'
 Mumukit.runner_name = 'elixir'
 Mumukit.configure do |config|
   config.docker_image = 'mumuki/mumuki-elixir-worker'
-
+  config.comment_type = Mumukit::Directives::CommentType::Ruby
+  config.stateful = true
 end
 
-require_relative 'test_hook'
-require_relative 'metadata_hook'
+require_relative './test_hook'
+require_relative './metadata_hook'
 require_relative './query_hook'
-require 'mumukit/server/app'
