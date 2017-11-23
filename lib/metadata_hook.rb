1d921aa5859c90b051bf876c1257b8210ba4656e
Julian Berbel Alt <julianberbel@hotmail.com>
2017-11-23 17:29:07 -0300

    Add exunit template to metadata

--- lib/metadata_hook.rb
+++ lib/metadata_hook.rb
@@ -10,7 +10,12 @@ class ElixirMetadataHook < Mumukit::Hook
      test_framework: {
          name: 'exunit',
          version: '1.3.0',
-         test_extension: '.exs'
+         test_extension: '.exs',
+         template: <<elixir
+test "{{ test_template_sample_description }}" do
+  assert true
+end
+elixir
      }}
   end
 end
